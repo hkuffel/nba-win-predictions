849831255e8fe07d43ae1e04be541755ab648926
Hunter Kuffel <hkuffel@alumni.nd.edu>
2019-09-17 22:04:55 -0400

    Improved web scraping script and added comp notebook

--- scrape.py
+++ scrape.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
 
-def stattaboy(year, stattype):
+# Function to scrape statistics from Basketball Reference into a DataFrame
+def get_stats(year, stattype):
     url = f"https://www.basketball-reference.com/leagues/NBA_{year}_{stattype}.html"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -13,38 +14,35 @@ def stattaboy(year, stattype):
     player_stats = [[td.getText() for td in rows[i].findAll('td')] for i in range(len(rows))]
     return pd.DataFrame(player_stats, columns=headers)
 
+# Function to merge together two dataframes, tailored to our specific use case
 def merge_frames(frame, aframe):
-    aframe = aframe.drop('Player', axis = 1)
-    aframe = aframe.drop('Pos', axis = 1)
-    aframe = aframe.drop('Age', axis = 1)
-    aframe = aframe.drop('Tm', axis = 1)
-    aframe = aframe.drop('G', axis = 1)
-    aframe = aframe.drop('MP', axis = 1)
-    aframe= aframe.drop('\xa0', axis = 1)
+    aframe = aframe.drop(['Player', 'Pos', 'Age', 'Tm', 'G', 'MP', '\xa0'], axis = 1)
     df = frame.join(aframe)
     return df
 
+# Function to stack the merged dataframes on top of each other for a range of years
 def yearsonyears(years):
     df = pd.DataFrame()
     for year in years:
-        frames = [stattaboy(year, 'totals'), stattaboy(year, 'advanced')]
+        frames = [get_stats(year, 'totals'), get_stats(year, 'advanced')]
         year_df = merge_frames(frames[0], frames[1])
         year_df['year'] = [f'{year}'] * len(year_df)
         df = df.append(year_df, ignore_index = True)
         df = df.dropna()
     return df
 
-df = yearsonyears(np.arange(2000,2020,1))
+df = yearsonyears(np.arange(1976,2020,1))
 
+# Changing necessary columns to numeric types
 cols = df.columns.to_list()
-cols.remove('Player')
-cols.remove('Pos')
-cols.remove('Tm')
-cols.remove('year')
-
+skip_col = ['Player', 'Pos', 'Tm', 'year']
 for col in cols:
-    df[col] = pd.to_numeric(df[col], errors = 'coerce')
+    if col in skip_col:
+        pass
+    else:
+        df[col] = pd.to_numeric(df[col], errors = 'coerce')
 
 df = df.dropna()
+df = df.reset_index(drop = True)
 
-df.to_csv('player_stats_2000-2019.csv', index=False)
+df.to_csv('player_stats_1976-2019.csv', index=False)
